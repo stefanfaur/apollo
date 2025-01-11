787220f0cc8f421ac8badff027108e16d608d5b9
stefanfaur <stefan.faur@student.upt.ro>
2025-01-11 21:50:47 +0200

    [frontend] add redirect to login when jwt expires

--- safr-mobile/safr/utils/apiClient.ts
+++ safr-mobile/safr/utils/apiClient.ts
@@ -1,21 +1,39 @@
 import axios from 'axios';
-import { getToken } from './secureStore';
+import { getToken, deleteToken } from './secureStore';
+import { router } from 'expo-router';
 
 const apiClient = axios.create({
-    baseURL: 'http://localhost:8080/api', // TODO: extract this
+    baseURL: 'http://localhost:8080/api', // TODO: extract this into an environment variable
 });
 
-// include jwt in all requests
+// include JWT in all requests
 apiClient.interceptors.request.use(
     async (config) => {
         const token = await getToken();
+        console.log('sent request with token:', token);
         if (token) {
             config.headers.Authorization = `Bearer ${token}`;
         }
         return config;
     },
     (error) => {
-        // TODO: properly handle errors
+        console.error("Request error:", error);
+        return Promise.reject(error);
+    }
+);
+
+apiClient.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        if (error.response?.status === 401) {
+            console.warn("Unauthorized! Redirecting to login.");
+
+            // clear stale token
+            await deleteToken();
+
+            // redirect to entry
+            router.replace('/(entry)');
+        }
         return Promise.reject(error);
     }
 );
