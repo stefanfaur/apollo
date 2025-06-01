c9612b107c9f033bdf53ef745b60181192ac2221
stefanfaur <stefan.faur@student.upt.ro>
2025-06-01 20:13:28 +0300

    [mobile] update url config

--- safr-mobile/safr/constants/config.ts
+++ safr-mobile/safr/constants/config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    BASE_URL: 'http://apollo.local',
-    API_URL: 'http://apollo.local/api',
+    BASE_URL: 'http://localhost:8080',
+    API_URL: 'http://localhost:8080/api',
 } as const; 
